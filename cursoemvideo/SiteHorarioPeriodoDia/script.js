49a45a8b4ee9a90e62ff9895c7e1147449cef072
xGuilhermeC <guilhermecavalcanti@discente.ufg.br>
2021-05-03 15:30:32 -0300

    Codigo do site finalizado.

--- cursoemvideo/SiteHorarioPeriodoDia/script.js
+++ cursoemvideo/SiteHorarioPeriodoDia/script.js
@@ -1,14 +1,17 @@
-    function carregar() {
+function carregar() {
     var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('foto')
+    var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 5 && hora < 12) {
-        img.src = 'imagens\manha.png'
-    }else if (hora >= 12 && hora < 18) {
-        img.src = 'imagens\tarde.png'
+        img.src = 'imagens/manha.png'
+        document.body.style.background = '#e2cd9f'
+    } else if (hora >= 12 && hora < 18) {
+        img.src = 'imagens/tarde.png'
+        document.body.style.background = '#b9846f'
     } else {
-        img.src = 'imagens\noite.png'
+        img.src = 'imagens/noite.png'
+        document.body.style.background = '#515154'
     }
 }
