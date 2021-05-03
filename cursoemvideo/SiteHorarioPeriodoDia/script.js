3d2b4d98e24278092e4511c7c0ec2b76551480b5
xGuilhermeC <guilhermecavalcanti@discente.ufg.br>
2021-05-03 00:22:42 -0300

    Inclusão de imagens no código

--- cursoemvideo/SiteHorarioPeriodoDia/script.js
+++ cursoemvideo/SiteHorarioPeriodoDia/script.js
@@ -1,1 +1,14 @@
-alert('Olá!')
+    function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('foto')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 5 && hora < 12) {
+        img.src = 'imagens\manha.png'
+    }else if (hora >= 12 && hora < 18) {
+        img.src = 'imagens\tarde.png'
+    } else {
+        img.src = 'imagens\noite.png'
+    }
+}
